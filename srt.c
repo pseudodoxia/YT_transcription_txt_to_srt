7a5747eaea50d8eaa6bf208fcb892df7f6cbf834
pseudodoxia <SternalArt@gmail.com>
2024-06-17 15:26:17 +0200

    Update srt.c

--- srt.c
+++ srt.c
@@ -11,7 +11,7 @@ int main() {
 	b = 0;
 	nr_sub = 0;
 
-	while (( c = getchar()) != EOF){	// this idea comes from 
+	while (( c = getchar()) != EOF){	// this idea comes from  The ANSI C Programming Language by Brian W. Kernighan (see Readme.md)
 		licznik = 0;
 		tab[a] = c;
 
